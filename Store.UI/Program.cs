b651de7a9de888ea673ac8626e274c4a0038a647
Craig Stuckey <craig@Craigs-MacBook-Pro.local>
2019-04-26 14:57:47 -0500

    Pushing the initial files. No implementation yet.

---
+++ Store.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Store.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
